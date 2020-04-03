60e5f8d0fcd766230a8fa2e7f7af831299a9d9b6
hirmaelena <hirmaelena@hotmail.com>
2020-04-02 19:57:26 -0600

    implementar nav con bootstrap react

--- src/components/teachers/MenuTeachers.js
+++ src/components/teachers/MenuTeachers.js
@@ -1,37 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { Nav, Navbar } from 'react-bootstrap'
 
-const MenuTeachers = ({setLoginTeacher, setLoginParent, login, setLogin}) => {
-  
-  const {fullName, userType} = login
+const MenuTeachers = ({ setLoginTeacher, setLoginParent, login, setLogin }) => {
 
-  const handleLogout = () =>{
+  const { fullName, userType } = login
 
-    if(userType === 'parent'){
+  const handleLogout = () => {
+
+    if (userType === 'parent') {
       setLoginParent(false)
       setLogin({})
-    }else if(userType === 'teacher'){
+    } else if (userType === 'teacher') {
       setLoginTeacher(false)
       setLogin({})
     }
   }
 
+  // Close nav when select a link
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <Fragment>
       <aside className="dash-menu col-md-2 col-sm-3">
-        <nav className="navbar navbar-expand-sm">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i className="fas fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <Navbar className="navbar" expand="lg">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
+          <Navbar.Collapse id="basic-navbar-nav">
             <ul className="dash-nav d-flex d-sm-none justify-content-between mb-5">
               <li className="text-left ml-0">
                 <i className="fas fa-user mr-1"></i>
@@ -40,34 +34,31 @@ const MenuTeachers = ({setLoginTeacher, setLoginParent, login, setLogin}) => {
                 </h5>
               </li>
               <li>
-                <Link to={"/DashboardTeachers"}><i className="fas fa-home"></i></Link>
-              </li>
-              <li>
                 <Link to=""><i onClick={handleLogout} className="fas fa-sign-out-alt"></i></Link>
               </li>
             </ul>
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <NavLink activeClassName="is-selected"  className="nav-link" to={'/DashboardTeachers'} >
+            <Nav className="navbar-nav" activeKey="/DashboardTeachers" as="ul">
+              <Nav.Item as="li">
+                <NavLink className="nav-link" activeClassName="active" onClick={() => setExpanded(false)} to={"/DashboardTeachers"}>
                   <i className="fas fa-calendar-alt fa-fw"></i>
                   <span>Calendario de Juntas</span>
                 </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink activeClassName="is-selected"  to={'/meetingHistoryTeachers'} className="nav-link">
+              </Nav.Item>
+              <Nav.Item as="li">
+                <NavLink className="nav-link" activeClassName="active" onClick={() => setExpanded(false)} eventKey="link-2" to="/meetingHistoryTeachers">
                   <i className="fas fa-folder-open fa-fw"></i>
                   <span>Historial de Juntas</span>
                 </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink activeClassName="is-selected" to={"/NewMeeting"} className="nav-link">
+              </Nav.Item>
+              <Nav.Item as="li">
+                <NavLink className="nav-link" activeClassName="active" onClick={() => setExpanded(false)} eventKey="link-3" to="/NewMeeting">
                   <i className="far fa-calendar-check fa-fw"></i>
                   <span>Agendar Nueva Junta</span>
                 </NavLink>
-              </li>
-            </ul>
-          </div>
-        </nav>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </aside>
     </Fragment>
   );
